613a54d28528deefbb746df2f1357f2d5448a6a4
Blessed Dominic <blesseddominic@Blesseds-MBP.lan>
2022-06-19 23:28:25 +0100

    decompose the monolith to microservices

--- set_env.sh
+++ set_env.sh
@@ -1,8 +1,19 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+# export POSTGRES_USERNAME=postgres
+# export POSTGRES_PASSWORD=mypassword
+# export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+# export POSTGRES_DB=postgres
+# export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+# export AWS_REGION=us-east-1
+# export AWS_PROFILE=default
+# export JWT_SECRET=testing
+# export URL=http://localhost:8100
+
+# my CREDENTIALS
+export POSTGRES_USERNAME=blisss
+export POSTGRES_PASSWORD=Makarios25
+export POSTGRES_HOST=cdr.crulkbye7cw5.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::mybucket1200798
 export AWS_REGION=us-east-1
