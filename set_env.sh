14640e2cae3cd390ac1474295df197c06d2516ef
Blessed538 <blesseddominic98@gmail.com>
2022-07-12 13:28:07 +0100

    login and got logs from the user

--- set_env.sh
+++ set_env.sh
@@ -1,25 +1,12 @@
-# This file is used for convenience of local development.
-# DO NOT STORE YOUR CREDENTIALS INTO GIT
-# export POSTGRES_USERNAME=postgres
-# export POSTGRES_PASSWORD=mypassword
-# export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-# export POSTGRES_DB=postgres
-# export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-# export AWS_REGION=us-east-1
-# export AWS_PROFILE=default
-# export JWT_SECRET=testing
-# export URL=http://localhost:8100
 
-# my CREDENTIALS
+
+# psql -h database-1.c30mxbao3ysz.us-east-2.rds.amazonaws.com-U blessed postgres
 export POSTGRES_USERNAME=blessed
 export POSTGRES_PASSWORD=Makarios25
-export POSTGRES_HOST=database-1.clqhfnhnjkkb.us-east-2.rds.amazonaws.com
+export POSTGRES_HOST=database-1.c30mxbao3ysz.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=microservicebuckets
+export AWS_BUCKET=microservicebucketings
 export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
-
-# access key id =ASIA5OBD7GD6F3AF32WT
-# access key token =eH0G2JI8jSJ80mibR0wwkm519Q4sMvI4J7ywwXwv
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a62892700be4a40f4a0778da877d04d3-584272316.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
